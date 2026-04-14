7421a52dce1ca4b425c2880598e5c6bac3bcb505
ornella28 <angelnibaruta88@gmail.com>
2026-04-14 14:45:35 +0200

    changed index.html title

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
 function App() {
-  return (
-      <div>
-        <h1>Welcome to My First React App</h1>
-        <p>I am learning React step by step 🚀</p>
-      </div>
-  )
+    return (
+        <div>
+            <h1>Welcome to My First React App</h1>
+            <p>I am learning React step by step 🚀</p>
+        </div>
+    )
 }
 
 export default App
