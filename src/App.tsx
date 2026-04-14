fd3c8c30b421e4fdece32b001c68538d29524461
ornella28 <angelnibaruta88@gmail.com>
2026-04-14 14:39:09 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+      <div>
+        <h1>Welcome to My First React App</h1>
+        <p>I am learning React step by step 🚀</p>
+      </div>
+  )
+}
+
+export default App
